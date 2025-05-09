f27ec69a85ed81ca3482935a0463447001fad037
yunx-z <yunxiang@umich.edu>
2025-05-09 08:34:35 -0400

    update

--- launch.sh
+++ launch.sh
@@ -2,14 +2,14 @@ model=$1
 dataset=$2
 experiment_id=$3
 
-alpha_strategy="constant"
+alpha_strategy="warmup"
 
 if [[ "$model" == *"small"* ]]; then
     gpu_cnt=1
-    batch_sz=8
+    batch_sz=1
 else
-    gpu_cnt=4
-    batch_sz=8
+    gpu_cnt=2
+    batch_sz=1
 fi
 
 if [[ "$dataset" == *"train"* ]]; then
@@ -27,15 +27,15 @@ do_sample="True"
 
 max_generation_token=16384
 
-job_name="${model}--${alpha_strategy}--${dataset}"
+job_name="${model}--${dataset}--${experiment_id}"
 echo """#!/bin/bash
 
-#SBATCH --account=wangluxy1
+#SBATCH --account=wangluxy_owned1
 #SBATCH --job-name=${job_name}       # Name of the job
 #SBATCH --output=logs/gl/${job_name}--%j.log   # File to which the output will be written
 #SBATCH --error=logs/gl/${job_name}--%j.log     # File to which the error will be written
 #SBATCH --time=07-00:00:00           # Wall time limit of the job (e.g., 1 hour)
-#SBATCH --partition=spgpu           # Partition (or queue) name
+#SBATCH --partition=spgpu2           # Partition (or queue) name
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:${gpu_cnt}              # Request 1 GPU
 #SBATCH --ntasks=1                # Number of tasks, typically set to 1 for single GPU jobs
@@ -61,21 +61,46 @@ my_data_dir=\"data/eval/\${dataset}/\"
 
 large_size=\"32\"
 small_size=\"1.5\"
-large_expert_model=\"deepseek-ai/DeepSeek-R1-Distill-Qwen-\${large_size}B\"
 small_rft_expert_model=\"checkpoints/small_rft_expert_model\"
+small_rft_lora_expert_model=\"checkpoints/small_rft_lora_expert_model\"
 small_pft_expert_model=\"checkpoints/small_pft_expert_model\"
+small_pft_lora_expert_model=\"checkpoints/small_pft_lora_expert_model\"
+small_short_ft_lora_expert_model=\"checkpoints/small_short_ft_lora_expert_model\"
+small_early_pft_expert_model=\"checkpoints/small_early_pft_expert_model\"
+small_50_pft_expert_model=\"checkpoints/small_50_pft_expert_model\"
 small_distill_expert_model=\"checkpoints/small_distill_expert_model\"
-if [[ \"${model}\" == \"dexperts-rft\" ]]; then
-	small_expert_model=\${small_rft_expert_model}
-elif [[ \"${model}\" == \"dexperts-distill\" ]]; then
-	small_expert_model=\${small_distill_expert_model}
-elif [[ \"${model}\" == \"dexperts-pft\" ]]; then
-	small_expert_model=\${small_pft_expert_model}
-else
-	small_expert_model=\"deepseek-ai/DeepSeek-R1-Distill-Qwen-\${small_size}B\"
+small_rl_expert_model=\"agentica-org/DeepScaleR-\${small_size}B-Preview\"
+small_expert_model=\"deepseek-ai/DeepSeek-R1-Distill-Qwen-\${small_size}B\"
+
+if [[ \"${model}\" == *\"dexperts\"* ]]; then
+	large_expert_model=\"deepseek-ai/DeepSeek-R1-Distill-Qwen-\${large_size}B\"
+	if [[ \"${model}\" == \"dexperts-rft\" ]]; then
+		small_expert_model=\${small_rft_expert_model}
+	elif [[ \"${model}\" == \"dexperts-distill\" ]]; then
+		small_expert_model=\${small_distill_expert_model}
+	elif [[ \"${model}\" == \"dexperts-pft\" ]]; then
+		small_expert_model=\${small_pft_expert_model}
+	elif [[ \"${model}\" == \"dexperts-early-pft\" ]]; then
+		small_expert_model=\${small_early_pft_expert_model}
+	elif [[ \"${model}\" == \"dexperts-50-pft\" ]]; then
+		small_expert_model=\${small_50_pft_expert_model}
+	elif [[ \"${model}\" == \"dexperts-rft-lora\" ]]; then
+		small_expert_model=\${small_rft_lora_expert_model}
+	elif [[ \"${model}\" == \"dexperts-pft-lora\" ]]; then
+		small_expert_model=\${small_pft_lora_expert_model}
+	elif [[ \"${model}\" == \"dexperts-short-ft-lora\" ]]; then
+		small_expert_model=\${small_short_ft_lora_expert_model}
+	else
+		small_expert_model=\"deepseek-ai/DeepSeek-R1-Distill-Qwen-\${small_size}B\"
+	fi
+	large_base_model=\"Qwen/Qwen2.5-\${large_size}B\"
+	small_base_model=\"Qwen/Qwen2.5-Math-\${small_size}B\"
+elif [[ \"${model}\" == *\"RLproxy\"* ]]; then
+	large_expert_model=\"\"
+	small_expert_model=\${small_rl_expert_model}
+	small_base_model=\"deepseek-ai/DeepSeek-R1-Distill-Qwen-\${small_size}B\"
+	large_base_model=\"deepseek-ai/DeepSeek-R1-Distill-Qwen-\${large_size}B\"
 fi
-large_base_model=\"Qwen/Qwen2.5-\${large_size}B\"
-small_base_model=\"Qwen/Qwen2.5-Math-\${small_size}B\"
 
 # large_size=\"72\"
 # small_size=\"1.5\"
@@ -92,20 +117,33 @@ small_base_model=\"Qwen/Qwen2.5-Math-\${small_size}B\"
 # small_base_model=\"meta-llama/Llama-3.1-\${small_size}B\"
 
 
-
-if [[ \"${model}\" == *\"dexperts\"* ]]; then
+if [[ \"${model}\" == *\"dexperts\"* || \"${model}\" == *\"RLproxy\"* ]]; then
 	results_dir=\"results/\${dataset}/${model}-S\${small_size}B-L\${large_size}B/${alpha_strategy}/\${experiment_id}\"
 	echo \"Results dir: \${results_dir}\"
-	python -m eval.gsm.run_eval \
-	    --max_new_tokens ${max_generation_token} \
-	    --data_dir \${my_data_dir} \
-	    --save_dir \${results_dir} \
-	    --base_model_name_or_path \${large_base_model} \
-	    --expert_model_name_or_path \${small_expert_model} \
-	    --anti_expert_model_name_or_path \${small_base_model} \
-	    --alpha_strategy \"${alpha_strategy}\" \
-	    --do_sample ${do_sample} \
-	    --eval_batch_size \${batch_sz}
+	while true; do
+		if [ -f \"\${results_dir}/predictions.jsonl\" ]; then
+			line_count=\$(wc -l < \"\${results_dir}/predictions.jsonl\")
+			if [ \"\$line_count\" -ge 30 ]; then
+				echo \"predictions.jsonl has 30 or more lines. Exiting loop.\"
+				break
+			fi
+			echo \"predictions.jsonl has \$line_count lines, still running evaluation...\"
+		else
+			echo \"predictions.jsonl does not exist yet. Running evaluation...\"
+		fi
+		python -m eval.gsm.run_eval \
+		    --max_new_tokens ${max_generation_token} \
+		    --data_dir \${my_data_dir} \
+		    --save_dir \${results_dir} \
+		    --base_model_name_or_path \${large_base_model} \
+		    --expert_model_name_or_path \${small_expert_model} \
+		    --anti_expert_model_name_or_path \${small_base_model} \
+		    --alpha_strategy \"${alpha_strategy}\" \
+		    --do_sample ${do_sample} \
+		    --eval_batch_size \${batch_sz}
+
+		sleep 5 # Wait for a few seconds before checking again
+	done
 elif [[ \"${model}\" == \"SBLELB\" ]]; then
 	results_dir=\"results/\${dataset}/SBLELB-S\${small_size}B-L\${large_size}B/${alpha_strategy}/\${experiment_id}\"
 	echo \"Results dir: \${results_dir}\"
--- launch_all.sh
+++ launch_all.sh
@@ -1,5 +1,5 @@
 # dataset_list=(MATH_hard_train_{00..48})
-dataset_list=("amc23") # "aime2024" "aime2025" "MATH_hard_test"
+dataset_list=("aime2024") # "aime2025" "amc23" "MATH_hard_test")
 
 for dataset in "${dataset_list[@]}"; do
 	if [[ "$dataset" == *"MATH_hard_train"* ]]; then
@@ -9,14 +9,24 @@ for dataset in "${dataset_list[@]}"; do
 	fi
 	# total_runs=1
 
+	# 1-4 spgpu 5-8 spgpu2
+	# for i in {5..8}; do
 	for i in $(seq 1 $total_runs); do
+		bash launch.sh "dexperts-short-ft-lora" "$dataset" "$i"
 		# spgpu2
-		bash launch.sh "dexperts" "$dataset" "$i"
+		# bash launch.sh "dexperts" "$dataset" "$i"
+		# bash launch.sh "dexperts-distill" "$dataset" "$i"
+		# bash launch.sh "dexperts-rft" "$dataset" "$i"
+		# bash launch.sh "dexperts-rft-lora" "$dataset" "$i"
+		# bash launch.sh "dexperts-pft-lora" "$dataset" "$i"
+		# bash launch.sh "RLproxy" "$dataset" "$i"
+		# bash launch.sh "small_rl_expert_model" "$dataset" "$i"
 
-		models=("small_expert_model" "large_base_model" "large_expert_model")
-		for model in "${models[@]}"; do
-			bash launch.sh "$model" "$dataset" "$i"
-		done
+		# models=("small_expert_model" "large_base_model" "large_expert_model")
+		# models=("small_rft_expert_model" "small_pft_expert_model" "small_distill_expert_model")
+		# for model in "${models[@]}"; do
+		# 	bash launch.sh "$model" "$dataset" "$i"
+		# done
 
 		# spgpu
 		# alpha_strategys=("constant0.25" "constant0.5" "constant2.0")
@@ -28,6 +38,5 @@ for dataset in "${dataset_list[@]}"; do
 		# for alpha_strategy in "${alpha_strategys[@]}"; do
 		# 	bash launch.sh "TwoBody" "${alpha_strategy}" "$dataset"
 		# done
-
 	done
 done
